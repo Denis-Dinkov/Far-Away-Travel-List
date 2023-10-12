24ceb1298011319b320481b295d723253750d94a
Denis Dinkov <denisdinkov7@gmail.com>
2023-10-12 23:02:53 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 function App() {
   return (
     <div className="app">
@@ -20,14 +25,24 @@ function Form() {
   );
 }
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <ul className="list">
+      {initialItems.map((item) => (
+        <Item item={item} />
+      ))}
+    </ul>
+  );
 }
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>You have x items on your list, and you alredy packed X</em>
     </footer>
   );
+}
+
+function Item({ item }) {
+  return <li>{item.description}</li>;
 }
 
 export default App;
